e70dc1891135e8ff460580d142b2a1fc28856d15
Marcelo Alves <marcelo.asf92@gmail.com>
2022-11-19 23:37:33 -0300

    fix: allow requests at the server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import "express-async-errors";
 import express from "express";
+import cors from "cors";
 import errorHandler from "./middlewares/errorHandler.middleware";
 import accountsRoutes from "./routes/accounts/accounts.routes";
 import transactionsRoutes from "./routes/transactions/transaction.routes";
 import usersRoutes from "./routes/users/users.routes";
-
 const app = express();
+
+/*To allow requests at the server */
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use(express.json());
 
